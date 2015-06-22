de09ac711d6660d3cb251472076f599ec7c0162a
Peter Szabo <asdwsda@gmail.com>
2015-06-22 02:54:54 +0200

    LogMessage: extend with setValue()

--- modules/java/LogMessage.java
+++ modules/java/LogMessage.java
@@ -34,6 +34,18 @@ public class LogMessage {
     return getValue(handle, name);
   }
 
+  public void setValue(String name, String value) {
+    if (name == null || name.isEmpty()) {
+      throw new IllegalArgumentException("Name cannot be empty");
+    }
+    if (value == null) {
+      throw new IllegalArgumentException("Value cannot be null");
+    }
+
+    setValue(handle, name, value);
+  }
+
+
   public void release() {
     unref(handle);
     handle = 0;
@@ -45,4 +57,5 @@ public class LogMessage {
 
   private native void unref(long handle);
   private native String getValue(long ptr, String name);
+  private native void setValue(long ptr, String name, String value);
 }
